7dcc7278a4cacdc9f0e515120c955435a8f64b7b
Trung Dinh Quang <trungdq88@gmail.com>
2015-12-24 00:39:12 +0700

    Logic works

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import gauss from '../third-party/gauss.js';
+import gauss_2 from '../third-party/gauss_2.js';
 import calc from './calc.js';
 
 export default class App extends React.Component {
@@ -7,6 +7,8 @@ export default class App extends React.Component {
     super(...args);
     this.state = {
       numbers: '',
+      answer: '',
+      func: '',
     };
   }
 
@@ -16,13 +18,76 @@ export default class App extends React.Component {
     });
   }
 
-  find() {
-    console.log(calc(this.state.numbers.split(' ').map(n => +n), gauss));
+  getValue(fraction) {
+    const sign = fraction.s === 1 ? '' : '-';
+    return fraction.d === 1 ? fraction.valueOf() : `${sign}${fraction.n}/${fraction.d}`;
   }
 
+  buildFuncString(params) {
+    let func = 'f(x) = ';
+
+    for (var i = 0; i < params.length; i++) {
+      let positive = params[i].s === 1;
+      let val = this.getValue(params[i]);
+      let pow = params.length - 1 -i;
+      const operator = positive ? '+' : '';
+
+
+      if (val === 0) continue;
+      if (val === 1 && pow !== 0) val = '';
+      if (pow === 0) {
+        func += `${operator} ${val} `;
+      } else if (pow === 1) {
+        func += `${operator} ${val}x `;
+      } else {
+        func += `${operator} ${val}x^${pow} `;
+      }
+    }
+
+    func = func.replace(/ = \+/, ' =');
+
+    return func;
+  }
+  find() {
+    const result = calc(this.state.numbers.split(' ').map(n => +n), gauss_2);
+    const number = this.getValue(result.result);
+
+    this.setState({
+      answer: number,
+      func: this.buildFuncString(result.params),
+    });
+  }
+
+  renderDescription() {
+    if (this.state.answer) {
+      return (
+        <div>
+          <h1>
+            The next number is {this.state.answer}
+          </h1>
+          <h3>Why?</h3>
+          <p>
+            Let <b>{this.state.func}</b>
+            <br/>
+            Then:
+            <br/>
+            <div>
+              {this.state.numbers.split(' ').map((number, i) => {
+                return <p key={i}>f({i + 1}) = {number}</p>;
+              })}
+              <b>f({this.state.numbers.split(' ').length + 1}) = {this.state.answer}</b>
+            </div>
+          </p>
+        </div>
+      );
+    } else {
+      return '';
+    }
+  }
   render() {
     return (
       <div>
+        <p>Enter your number sequence (Example: "1 2 3 4")</p>
         <p>
           <input
             type="text"
@@ -37,6 +102,7 @@ export default class App extends React.Component {
         >
           Find next number
         </button>
+        {this.renderDescription()}
       </div>
     );
   }
